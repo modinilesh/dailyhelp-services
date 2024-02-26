ef5d30b52d72fcddef3b5cb5526eaeaf924b0240
Nilesh Modi <nilesh.modi.nitb@gmail.com>
2024-02-26 19:17:30 +0530

    initialized-project-spring-initializer

---
+++ src/test/java/com/homecare/services/HomeCareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.homecare.services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeCareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
